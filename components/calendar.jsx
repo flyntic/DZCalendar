2d36018b0443252ff2a76c5e8ecb52305556ee1b
flyntic <64376713+flyntic@users.noreply.github.com>
2023-09-29 11:38:35 +0300

    Update calendar.jsx
     Поправлен онклик

--- components/calendar.jsx
+++ components/calendar.jsx
@@ -1,26 +1,16 @@
-
-function Day(day,index,_OnClick)
-{console.log(day);
-  return (<td key={index}>
-          <input type="button" value={day.val} className={day.disableday}
-                 OnClick={_OnClick}/>
+function Day(day,OnClick)
+{
+  return (<td >
+          <input type="button" value={day.val} className={day.disableday+" "+day.select}
+                 onClick={OnClick}/>
           </td>);
  
 }
-/*
-function WeekStr(_week) {
-   return (
-   <tr>
-   {_week.map(day=>Day(day))}   
-   </tr>
-   );
-}
-*/
 
-export default function Calendar({Month,_OnClick})
+export default function Calendar({Month,OnClick})
 {      
      return  (
       <table> 
-       {Month.map( (week,index) => <tr key={index}>{week.map((day,index)=>Day(day,index,_OnClick))}</tr> ) }    
+       {Month.map( (week,index) => <tr key={index}>{week.map((day,index)=>Day(day,OnClick))}</tr> ) }    
       </table>);
 }
